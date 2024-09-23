034064607ed070a4fbf4057b665b995a2fc1ff37
gordeevigor <mantraspirit@gmail.com>
2024-09-23 21:05:08 +0400

    1. Minor changes in Cat & Student classes

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,15 +1,25 @@
 package ru.job4j.oop;
 
 public class Cat {
-    public String sound() {
-        String voice = "may-may";
-        return voice;
+    private String food;
+    private String name;
+
+    void show() {
+        System.out.println("Котик " + this.name + " весь день ел " + this.food);
+    }
+
+    void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        Cat kotik = new Cat();
+        kotik.giveNick("Бармалей");
+        kotik.eat("Кильку");
+        kotik.show();
     }
 }
