e19e1cb6a8adc848bd6608965b677c579616f43e
gordeevigor <mantraspirit@gmail.com>
2024-09-25 15:26:23 +0400

    1. Add class Error

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Активность устройства: " + active);
+        System.out.println("Статус устройства: " + status);
+        System.out.println("Ответ устройства: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error tablet = new Error(true, 1, "Ошибок не обнаружено");
+        tablet.printInfo();
+        Error smartphone = new Error(false, 2, "Разряд батареи");
+        smartphone.printInfo();
+        Error monitor = new Error(false, 0, "Отсутсвует сингал");
+        monitor.printInfo();
+    }
+}
