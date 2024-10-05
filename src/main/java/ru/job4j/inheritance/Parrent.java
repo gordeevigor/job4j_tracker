38032ab0b91db53c31570abc2e0f609414dcc87e
gordeevigor <mantraspirit@gmail.com>
2024-10-05 11:24:03 +0400

    1.1. Inheritance

---
+++ src/main/java/ru/job4j/inheritance/Parrent.java
@@ -0,0 +1,11 @@
+package ru.job4j.inheritance;
+
+public class Parrent {
+    private String name;
+    private int age;
+
+    public Parrent(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
