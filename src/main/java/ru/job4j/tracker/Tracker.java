a4796a9f06766dedd51583d2b814941b3cf7b183
gordeevigor <mantraspirit@gmail.com>
2024-10-10 21:00:44 +0400

    1.1 Add methods in class Tracker

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -25,6 +27,25 @@ public class Tracker {
 
     public Item[] findAll() {
         Item[] result = items;
-        return result;
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (result[i] != null) {
+                result[size] = items[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(result, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = items;
+        int size = 0;
+        for (Item item : items) {
+            if (item != null && key.equals(item.getName())) {
+                result[size] = item;
+                size++;
+            }
+        }
+        return Arrays.copyOf(result, size);
     }
 }
