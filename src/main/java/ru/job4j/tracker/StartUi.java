f53a95f7f570c3644cd1ff30ee044934c7193c5a
gordeevigor <mantraspirit@gmail.com>
2024-10-14 20:28:08 +0400

    1.1 Using toString method for String variables console output.

--- src/main/java/ru/job4j/tracker/StartUi.java
+++ src/main/java/ru/job4j/tracker/StartUi.java
@@ -1,14 +1,8 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class StartUi {
     public static void main(String[] args) {
-        Item item = new Item();
-        LocalDateTime localDateTime = item.getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeFormatted = localDateTime.format(formatter);
-        System.out.println(currentDateTimeFormatted);
+        Item item = new Item(1, "JohnDoe");
+        System.out.println(item);
     }
 }
