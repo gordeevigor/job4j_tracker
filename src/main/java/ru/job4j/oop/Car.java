cd54e292e44edb8c42ea6cd38af62763ce888fbc
gordeevigor <mantraspirit@gmail.com>
2024-10-26 14:42:03 +0400

    1.Castings

--- src/main/java/ru/job4j/oop/Car.java
+++ src/main/java/ru/job4j/oop/Car.java
@@ -1,9 +1,13 @@
 package ru.job4j.oop;
 
-public class Car {
+public class Car extends Transport {
 
     private String brand;
     private String model;
+
+    public Car() {
+
+    }
 
     public Car(String brand, String model) {
         this.brand = brand;
