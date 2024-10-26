53394c6d6f1f5d1799a86aa7c524c85fbf0b45d2
gordeevigor <mantraspirit@gmail.com>
2024-10-26 13:37:16 +0400

    1.Created Bus class implements Transport interface

---
+++ src/main/java/ru/job4j/polymorphism/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.polymorphism;
+
+public class Bus implements Transport {
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void passengers(int count) {
+
+    }
+
+    @Override
+    public int refuel(int fuel) {
+        return 0;
+    }
+}
