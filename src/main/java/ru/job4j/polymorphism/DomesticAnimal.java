fea53edcfe08deab6f6ffb1a2d338cb7c3ee1032
gordeevigor <mantraspirit@gmail.com>
2024-10-27 16:05:12 +0400

    1. Casting

---
+++ src/main/java/ru/job4j/polymorphism/DomesticAnimal.java
@@ -0,0 +1,7 @@
+package ru.job4j.polymorphism;
+
+public class DomesticAnimal extends Animal {
+    public void liveOnFarm() {
+        System.out.println(nameClass + " живет на ферме, приносит пользу.");
+    }
+}
