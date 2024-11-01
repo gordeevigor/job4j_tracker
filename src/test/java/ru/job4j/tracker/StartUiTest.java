6f00f2e147f8614a090ad4ce4ab303321cc03ede
gordeevigor <mantraspirit@gmail.com>
2024-11-01 19:37:31 +0400

    1. Add test method whenReplaceItem checked array of items. 2. Add test method whenDeleteItem checked empty array and null in tracker find by id

--- src/test/java/ru/job4j/tracker/StartUiTest.java
+++ src/test/java/ru/job4j/tracker/StartUiTest.java
@@ -18,4 +18,28 @@ class StartUiTest {
         Item expected1 = new Item("Create token");
         assertThat(created1.getName()).isEqualTo(expected1.getName());
     }
+
+    @Test
+    void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId()), "edited item"};
+        StartUi.replaceItem(new MockInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("edited item");
+    }
+
+    @Test
+    void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answer = {String.valueOf(item.getId())};
+        StartUi.deleteItem(new MockInput(answer), tracker);
+        Item deleted = tracker.findById(item.getId());
+        Item[] erased = tracker.findAll();
+        assertThat(deleted).isNull();
+        assertThat(erased).isEmpty();
+    }
 }
