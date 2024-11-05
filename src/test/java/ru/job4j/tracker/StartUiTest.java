fbcda6ca8cfa61f099ba1ed3d7a4ff88f8515641
gordeevigor <mantraspirit@gmail.com>
2024-11-05 12:15:31 +0400

    1. Add Unit-test methods for FindAllAction, FindByNameAction, FindByIdAction classes.

--- src/test/java/ru/job4j/tracker/StartUiTest.java
+++ src/test/java/ru/job4j/tracker/StartUiTest.java
@@ -99,7 +99,7 @@ class StartUiTest {
         Output output = new StubOutput();
         Tracker tracker = new Tracker();
         Item one = tracker.add(new Item("test1"));
-        Input input = new MockInput(new String[] {"0", String.valueOf(one.getId()), "1"});
+        Input input = new MockInput(new String[] {"0", "1"});
         UserAction[] actions = new UserAction[] {
                 new FindAllAction(output),
                 new ExitAction(output)
@@ -139,6 +139,31 @@ class StartUiTest {
                         + one + ln
                         + "Меню:" + ln
                         + "0. Показать заявки по имени" + ln
+                        + "1. Завершить программу" + ln
+                        + "=== Завершение программы ===" + ln
+        );
+    }
+
+    @Test
+    void whenFindByIdActionTestOutputIsSuccessfully() {
+        Output output = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        Input input = new MockInput(new String[] {"0", String.valueOf(one.getId()), "1"});
+        UserAction[] actions = new UserAction[] {
+                new FindByIdAction(output),
+                new ExitAction(output)
+        };
+        new StartUi(output).init(input, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(output.toString()).isEqualTo(
+                "Меню:" + ln
+                        + "0. Показать заявку по id" + ln
+                        + "1. Завершить программу" + ln
+                        + "=== Вывод заявки по id ===" + ln
+                        + one + ln
+                        + "Меню:" + ln
+                        + "0. Показать заявку по id" + ln
                         + "1. Завершить программу" + ln
                         + "=== Завершение программы ===" + ln
         );
