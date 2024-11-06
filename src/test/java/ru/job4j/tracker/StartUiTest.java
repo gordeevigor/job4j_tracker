0c647370b8da9421d35307978e708cb04312dc09
gordeevigor <mantraspirit@gmail.com>
2024-11-06 13:16:03 +0400

    1. Factorial exception

--- src/test/java/ru/job4j/tracker/StartUiTest.java
+++ src/test/java/ru/job4j/tracker/StartUiTest.java
@@ -131,7 +131,7 @@ class StartUiTest {
         Output output = new StubOutput();
         Tracker tracker = new Tracker();
         Item one = tracker.add(new Item("test1"));
-        Input input = new MockInput(new String[] {"0", String.valueOf(one.getName()), "1"});
+        Input input = new MockInput(new String[] {"0", one.getName(), "1"});
         UserAction[] actions = new UserAction[] {
                 new FindByNameAction(output),
                 new ExitAction(output)
