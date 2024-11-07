db81b181229e391217c0ee514a73a07b4f0be68d
gordeevigor <mantraspirit@gmail.com>
2024-11-07 13:20:53 +0400

    1. Throw exception unit-test for Count class

---
+++ src/test/java/ru/job4j/ex/CountTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.ex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CountTest {
+    @Test
+    public void whenException() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    Count.add(10, 2);
+                }
+        );
+        assertThat(exception.getMessage()).isEqualTo("Start should be less than finish.");
+    }
+
+}
