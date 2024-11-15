2408c4a1eb98a4b878ee9573662fad7d913b3370
gordeevigor <mantraspirit@gmail.com>
2024-11-15 20:49:39 +0400

    1. Variant one - FinalUsage for variables and fields

---
+++ src/main/java/ru/job4j/ooa/FinalUsageLocalVariable.java
@@ -0,0 +1,8 @@
+package ru.job4j.ooa;
+
+public class FinalUsageLocalVariable {
+    public static void main(String[] args) {
+        final String NAME = "Petr";
+        System.out.println(NAME);
+    }
+}
