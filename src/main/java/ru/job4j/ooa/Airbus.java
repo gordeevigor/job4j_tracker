32fcf94420e53e447f18147715984848dfd984bd
gordeevigor <mantraspirit@gmail.com>
2024-11-16 12:49:48 +0400

    1. [Fix] Constant A380_COUNT_ENGINE transferred and renamed as class field COUNT_ENGINE_A380

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -2,6 +2,7 @@ package ru.job4j.ooa;
 
 public final class Airbus extends Aircraft {
     private static final int COUNT_ENGINE = 2;
+    private static final int COUNT_ENGINE_A380 = 4;
     private String name;
 
     public Airbus(String name) {
@@ -22,9 +23,8 @@ public final class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        final int A380_COUNT_ENGINE = 4;
         System.out.println("Количество двигателей равно: "
-                + ("A380".equals(name) ? A380_COUNT_ENGINE : COUNT_ENGINE));
+                + ("A380".equals(name) ? COUNT_ENGINE_A380 : COUNT_ENGINE));
     }
 
     @Override
