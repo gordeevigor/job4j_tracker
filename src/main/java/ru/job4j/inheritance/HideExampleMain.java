55628e849066217efd039c3f78e74163d9fa8d6b
gordeevigor <mantraspirit@gmail.com>
2024-11-26 16:57:17 +0400

    1. Exam prepare. Static override and hide

--- src/main/java/ru/job4j/inheritance/HideExampleMain.java
+++ src/main/java/ru/job4j/inheritance/HideExampleMain.java
@@ -4,14 +4,7 @@ public class HideExampleMain {
     public static void main(String[] args) {
         Animal animal = new Animal();
         Animal cat = new Cat();
-        animal.instanceInvoke();
-        cat.instanceInvoke();
-        Animal.staticInvoke();
         Cat.staticInvoke();
-        cat.staticInvoke();
-        Animal other = null;
-        other.staticInvoke();
-        Cat kitty = new Cat();
-        kitty.staticInvoke();
+        Animal.staticInvoke();
     }
 }
